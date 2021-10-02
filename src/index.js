58948de35f5114d90c9c9c85913fe918790a8366
Jonas Rafael Duarte dos Santos <jonasrdsantos@gmail.com>
2021-10-02 15:12:36 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: proces.env.SERVER_PORT || 8080 });
 
 const rooms = {};
 
